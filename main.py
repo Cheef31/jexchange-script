d1ec8acfc9ca1c6890fd0f6ddf21ce4624c6a844
Cheef31 <waleed_ahmed@hotmail.de>
2022-10-27 14:28:31 +0200

    added Discord Webhook and check for second cheapest price

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ def init():
     logging.basicConfig(filename='PriceDrop.log', encoding='utf-8', level=logging.INFO,
                         format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
+def postMessageToDiscord(message):
+    # Webhook URL
+    discordUrl = 'https://discordapp.com/api/webhooks/1035155414653878272/6m_Mvf516qLbxA9RGHRRJRzq1RE3TgWORQQ3pXdyHLD3dd_m56LEZEAkj-P04B4LaB5l'
+
+    data = {
+        "content": message,
+        "username": "Price Drop"
+    }
+
+    result = requests.post(discordUrl, json=data)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+
 def get_wegld_price():
     url = 'https://microservice.jexchange.io/prices/WEGLD-bd4d79'
     response = requests.get(url)
@@ -19,38 +37,38 @@ def get_allOffers(wegld_price, price_limit_to_notify):
     response = requests.get(url)
     response.raise_for_status() # raise exception if invalid response
     jsondata = response.json()
-    #pprint.pprint(jsondata)
+    #pprint.pprint(jsondata) # prettyprint the json
 
     # Get Token String
     separator = '-'
     token_to_buy = jsondata[0]["token_a_identifier"].split(separator, 1)[0]
-    token_to_buy_with = jsondata[0]["token_b_identifier"].split(separator, 1)[0]
 
-    # Always log cheapest price
-    logging.info(f'Cheapest Price: {round(jsondata[0]["rate"]*wegld_price, 4)} $')
+    cheapest_price = round(jsondata[0]["rate"]*wegld_price, 4)
+    # Also check second cheapest price for more reliability
+    second_cheapest_price = round(jsondata[1]["rate"]*wegld_price, 4)
 
-    # Print
-    print('Prices for 1 ' + token_to_buy + ' in ' + token_to_buy_with + ' on JEXchange.io')
-    print('----------------------------------------')
-    for x in jsondata:
-        r = x["rate"]
-        r_USD = r * wegld_price
-        print(f'{round(r_USD, 4)} $')
+    # Always print & log cheapest price
+    print(f'Current Price for 1 {token_to_buy}: {cheapest_price} $ ')
+    logging.info(f'Cheapest Price: {cheapest_price} $')
+    logging.info(f'Second cheapest Price: {second_cheapest_price} $')
 
-        if(r_USD < price_limit_to_notify):
-            print('LOG PRICE DROP')
-            logging.info(f'PRICE DROP under {price_limit_to_notify} $ to: {r_USD} $')
+    if(cheapest_price < price_limit_to_notify):
+        message = f'{token_to_buy} (< {price_limit_to_notify} $) = {cheapest_price} $'
+        postMessageToDiscord(message)
+    elif(second_cheapest_price < price_limit_to_notify):
+        message = f'{token_to_buy} (< {price_limit_to_notify} $) = {second_cheapest_price} $'
+        postMessageToDiscord(message)
 
 if __name__ == '__main__':
     init()
-    # price_limit_to_notify = float(input("Preisgrenze ab wann gelogged wird: "))
-    price_limit_to_notify = 0.07
+    #price_limit_to_notify = float(input("Preisgrenze ab wann gelogged wird: "))
+    price_limit_to_notify = 0.10
     sleeptime = 10
     while True:
         print('')
+        print('----------START----------')
         wegld_price = get_wegld_price()
         print(f'1 WEGLD = {round(wegld_price, 2)} $')
-        print('----------START----------')
         get_allOffers(wegld_price, price_limit_to_notify)
         print('----------END----------')
         for x in range(sleeptime):
